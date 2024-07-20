745e210f5f557382463873d6aba793076936b380
Gerald Todd <beingforthebenefit@gmail.com>
2024-07-20 14:15:36 -0700

    Initialize backend

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata"
+import { ApolloServer } from "apollo-server-express"
+import Express from "express"
+import { buildSchema } from "type-graphql"
+import { HelloResolver } from "./resolvers/HelloResolver"
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  })
+
+  const server = new ApolloServer({ schema })
+  await server.start()
+
+  const app = Express()
+  server.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log("Server is running on http://localhost:4000/graphql")
+  })
+}
+
+main()
