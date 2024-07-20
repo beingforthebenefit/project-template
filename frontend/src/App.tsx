8bf210e5a056ee9a5e476aafd74c932c899ff17b
Gerald Todd <beingforthebenefit@gmail.com>
2024-07-20 14:07:00 -0700

    Initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="default">Hello, world!</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App
