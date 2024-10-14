eafe283f5040d373aecc8738a88eb7f5b88d37e4
Gerald Todd <beingforthebenefit@gmail.com>
2024-10-13 18:41:33 -0700

    Fix mobile menu placement

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
         </ul>
         <ul>
           {isMobile && (
-            <li style={{ marginRight: '50px' }}>
+            <li>
               <button
                 aria-label="open mobile menu"
                 onClick={() => setMobileOpen(!mobileOpen)}
